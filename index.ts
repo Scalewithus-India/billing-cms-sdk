da9fbe73f999aaf546d1e7e573e1150a32574b18
Bubun Das <bubundas17@gmail.com>
2024-10-08 00:04:51 +0530

    Update version to 0.0.30
    Updated files:
    - index.ts
    - package.json

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 import type { NextFunction, Request, Response } from "express"
-import InvoiceModel from "./models/invoice.model"
-import UserModel from "./models/user.model"
-import { TicketModel } from "./models/tickets.model"
-import CurrencyModel from "./models/currency.model"
-import OptionModel from "./models/option.model"
-import ProductModel from "./models/product.model"
-import { EmailTemplateModel } from "./models/email-template.model" // Fixed typo in file name
-import ProductGroupModel from "./models/product-group.model"
-import { APIKeyModel } from "./models/api-keys.model"
-import AuthenticationMethodModel from "./models/authentication-method.model"
-import CountryModel from "./models/country-state.model"
+import type InvoiceModel from "./models/invoice.model"
+import type UserModel from "./models/user.model"
+import type { TicketModel } from "./models/tickets.model"
+import type CurrencyModel from "./models/currency.model"
+import type OptionModel from "./models/option.model"
+import type ProductModel from "./models/product.model"
+import type { EmailTemplateModel } from "./models/email-template.model" // Fixed typo in file name
+import type ProductGroupModel from "./models/product-group.model"
+import type { APIKeyModel } from "./models/api-keys.model"
+import type AuthenticationMethodModel from "./models/authentication-method.model"
+import type CountryModel from "./models/country-state.model"
 
 export interface PaymentGatewayConfig {
     name: string
@@ -37,7 +37,7 @@ export interface Lib {
     models: Models
 }
 
-export declare abstract class PaymentGateway { 
+export declare abstract class PaymentGateway {
     name: string
     icon: string
     constructor(lib: Lib);
